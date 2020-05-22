3fe3b80eb3504bca76c56c4a75dffff289876f31
Saintt0 <58036125+Saintt0@users.noreply.github.com>
2020-05-22 00:02:52 -0300

    Att6

--- src/main/java/com/example/atividade3_silvio_180467/controller/AutorController.java
+++ src/main/java/com/example/atividade3_silvio_180467/controller/AutorController.java
@@ -2,6 +2,7 @@ package com.example.atividade3_silvio_180467.controller;
 
 import com.example.atividade3_silvio_180467.service.AutorService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class AutorController {
     
+    @Autowired
     private AutorService aservice;
 
     @GetMapping("/autores")
